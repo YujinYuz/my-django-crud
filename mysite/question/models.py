36b847f425a29c9b4f1b4652a7fca1a690be96d5
Yujin <jinyuzprodigy@gmail.com>
2017-09-25 21:26:48 +0800

    Initial commit for mysite.

---
+++ mysite/question/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+	question_text = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return "{question_text}".format(question_text=self.question_text)
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=255)
+
+	def __str__(self):
+		return "{choice_text}".format(choice_text=self.choice_text)
+
